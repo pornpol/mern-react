6a7c3bb5b0e5c16cabd1c48d1e18e8a8b7e03c8c
pornpol <pornpol.w@gmail.com>
2020-01-23 23:09:26 +0700

    Add shared components

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+import UserList from "../components/UsersList";
+
 const Users = () => {
-  return <h2>User Works!</h2>;
+  const USER = [
+    {
+      id: "u1",
+      name: "Pornpol Wasuwat",
+      image: "https://www.9pol.dev/images/9pol.jpg",
+      places: 2
+    }
+  ];
+
+  return <UserList items={USER} />;
 };
 
 export default Users;
